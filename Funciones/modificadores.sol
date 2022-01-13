85204c4f5044e66c7068a5ab032769e6006f50f8
Gabriel Ramirez <gabriel.ramirez@salud.gob.sv>
2022-01-13 14:41:04 -0600

    modificadores

---
+++ Funciones/modificadores.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.4.4; 
+
+contract view_pure_payable{
+
+    string [] alumnos; 
+
+    function agregaAlumno(string memory _nombre) public {
+        alumnos.push(_nombre);
+    }
+
+    function verAlumno(uint _posicion) public view returns(string memory){
+        return alumnos[_posicion];
+    }
+
+    uint numero=10;
+
+    function sumar(uint _valor) public view returns(uint){
+        return numero+_valor; 
+    } 
+
+    function potencia(uint _base, uint _exponente) public pure returns(uint){
+        return _base**_exponente; 
+    }
+
+
+    //modificador payable 
+    mapping (address => cartera) DineroCartera;
+
+    struct cartera{
+        string nombre; 
+        address direccion;
+        uint cantidad; 
+    }
+
+    function pagar(string memory _nombre, uint _cantidad) public payable{
+        cartera memory mi_cartera; 
+        mi_cartera = cartera(_nombre,msg.sender,_cantidad); 
+        DineroCartera[msg.sender] = mi_cartera;
+    }
+
+    function verSaldo() public view returns(cartera memory){
+        return DineroCartera[msg.sender]; 
+    }
+
+}
--- Funciones/returns.sol
+++ Funciones/returns.sol
@@ -10,4 +10,11 @@ contract retornos{
     function return_multiple() public view returns(string memory, uint, address){
         return ("Hola mundo", 77, msg.sender); 
     }
+
+    function mostrarValores() public view{
+        string memory saludo;
+        uint numero;
+        address direccion; 
+        (saludo,numero,direccion) = return_multiple();
+    }
 }
